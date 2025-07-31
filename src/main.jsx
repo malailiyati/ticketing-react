53b891f030007e959b4f89a75a3754a1e003d88c
unknown <malailiyati107@gmail.com>
2025-07-31 09:29:00 +0700

    refactor: update entry point to use createRoot and Router component

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,9 @@
-import { StrictMode, useState } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./styles/index.css";
-// import App from "./components/App.jsx";
-import Login from "./components/Login.jsx";
-import Register from "./components/Register.jsx";
-import Movies from "./components/Movies.jsx";
+import Router from "./Router";
 
-// eslint-disable-next-line react-refresh/only-export-components
-function Home() {
-  const [page, setPage] = useState("login");
-  function changeHandler(event) {
-    setPage(event.target.value);
-  }
-  return (
-    <StrictMode>
-      <select name="page" value={page} onChange={changeHandler}>
-        <option value="login">Login</option>
-        <option value="register">Register</option>
-        <option value="movies">Movies</option>
-      </select>
-      {page === "login" && <Login />}
-      {page === "register" && <Register />}
-      {page === "movies" && <Movies />}
-    </StrictMode>
-  );
-}
-
-createRoot(document.getElementById("root")).render(<Home />);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Router />
+  </StrictMode>
+);
