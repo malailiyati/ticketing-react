a7d7514035a651623d5ef9e4b9e1880cf1f7374a
unknown <malailiyati107@gmail.com>
2025-08-18 17:23:51 +0700

    feat: add provider,persist, and store

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Router from "./Router";
+import { Provider } from "react-redux";
+import store, { persistedStore } from "../redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import AuthProvider from "./context/auth/AuthProvider";
+import HistoryProvider from "./context/ticket/HistoryProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistedStore}>
+        <AuthProvider>
+          <HistoryProvider>
+            <Router />
+          </HistoryProvider>
+        </AuthProvider>
+      </PersistGate>
+    </Provider>
   </StrictMode>
 );
