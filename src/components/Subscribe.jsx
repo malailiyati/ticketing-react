1b521b0a6070e9bd53053fa17a2522fada6fe9f5
unknown <malailiyati107@gmail.com>
2025-08-18 16:56:39 +0700

    feat:add seubscribe component for home and movie page

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -2,30 +2,37 @@ import React from "react";
 
 function Subscribe() {
   return (
-    <>
-      <section>
-        <div>
-          <h2>Subscribe to our newsletter</h2>
+    <section className="px-4 md:px-[70px] py-12">
+      <div className="relative bg-[var(--color--primary)] md:p-[50px] mx-[70px] rounded-[30px] items-center text-center h-fit overflow-hidden  py-[30px]">
+        <h2 className="text-white mb-[70px]">Subscribe to our newsletter</h2>
+        <div className="flex justify-center flex-col md:flex-row gap-[20px]">
           <div>
-            <div>
-              <label htmlFor="name"></label>
-              <input type="text" id="name" name="name" value="First name" />
-            </div>
-            <div>
-              <label htmlFor="email"></label>
-              <input
-                type="text"
-                id="email"
-                name="email"
-                value="Email address"
-              />
-            </div>
-            <button>Subscribe Now</button>
+            <label for="name"></label>
+            <input
+              className="py-[10px] px-[30px] text-white border border-gray-300 rounded-[5px] md:w-auto"
+              type="text"
+              id="name"
+              name="name"
+              value="First name"
+            />
           </div>
-          <div></div>
+          <div>
+            <label for="email"></label>
+            <input
+              className="py-[10px] px-[30px] text-white border border-gray-300 rounded-[5px] md:w-auto"
+              type="text"
+              id="email"
+              name="email"
+              value="Email address"
+            />
+          </div>
+          <button className="px-[30px] py-[10px] text-[var(--color--primary)] bg-white rounded-[5px] w-fit md:w-full mx-auto">
+            Subscribe Now
+          </button>
         </div>
-      </section>
-    </>
+        <div className="absolute w-[150px] h-[150px] rounded-full border-[5px] border-white top-40 left-220"></div>
+      </div>
+    </section>
   );
 }
 
